3984c9d5c5ae5e462a956879c2db350814c2d05d
SirioRedGiant <Oiris1992@gmail.com>
2026-02-02 18:14:22 +0100

    metodo alternativo

--- ciclo-for-tradizionale/FOR-TRAD-ARRAY/script.js
+++ ciclo-for-tradizionale/FOR-TRAD-ARRAY/script.js
@@ -1,6 +1,6 @@
 /**
  * LIVELLO 3.1: FONDAMENTA DEGLI ARRAY
- * 
+ *
  * Obiettivo: Imparare a leggere e scrivere negli array usando il ciclo for.
  */
 
@@ -10,17 +10,16 @@ const nomi = ["Edoardo", "Simone", "Francesco"];
 
 // --- Scrivi qui il codice ---
 for (let i = 0; i < nomi.length; i++) {
-    console.log("Benvenuto " + nomi[i] + "!");    
+  console.log("Benvenuto " + nomi[i] + "!");
 }
 
-
 // -----------------------------------------------------------------------------
-console.log("=============================================================================================================================================================");
-
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 // EX 2: COPIA SELETTIVA
-// Obiettivo: Cicla l'array 'numeri'. Se il numero è MAGGIORE di 10, 
+// Obiettivo: Cicla l'array 'numeri'. Se il numero è MAGGIORE di 10,
 // inseriscilo nell'array 'numeriGrandi' usando .push().
 // Alla fine stampa 'numeriGrandi'.
 const numeri = [5, 12, 8, 20, 3, 15];
@@ -29,20 +28,19 @@ const numeriGrandi = [];
 // --- Scrivi qui il codice ---
 
 for (let i = 0; i < numeri.length; i++) {
-    if (numeri[i] > 10) {
-        numeriGrandi.push(numeri[i])
-    }
+  if (numeri[i] > 10) {
+    numeriGrandi.push(numeri[i]);
+  }
 }
 console.log(numeriGrandi);
 
-
 // -----------------------------------------------------------------------------
-console.log("=============================================================================================================================================================");
-
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 // EX 3: TRASFORMAZIONE (RADDOPPIO)
-// Obiettivo: Crea un nuovo array 'raddoppiati'. 
+// Obiettivo: Crea un nuovo array 'raddoppiati'.
 // Cicla l'array 'base' e inserisci nel nuovo array il valore moltiplicato per 2.
 const base = [1, 2, 3, 4, 5];
 const raddoppiati = [];
@@ -50,14 +48,14 @@ const raddoppiati = [];
 // --- Scrivi qui il codice ---
 
 for (let i = 0; i < base.length; i++) {
-    raddoppiati.push(base[i] * 2)
+  raddoppiati.push(base[i] * 2);
 }
 console.log(raddoppiati);
 
 // -----------------------------------------------------------------------------
-console.log("=============================================================================================================================================================");
-
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 // EX 4: IL CONTEGGIO DEGLI ELEMENTI
 // Obiettivo: Dato l'array 'oggetti', conta quante volte appare la parola "Mela".
@@ -68,21 +66,22 @@ let contaMele = 0;
 // --- Scrivi qui il codice ---
 
 for (let i = 0; i < cesto.length; i++) {
-    if (cesto[i] === "Mela") {
-        contaMele += 1
-    }
+  if (cesto[i] === "Mela") {
+    contaMele += 1;
+  }
 }
 console.log(contaMele);
 
-console.log("=============================================================================================================================================================");
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 /**
  * LIVELLO 3.2: LOGICA APPLICATA AGLI ARRAY
-*/
+ */
 
 // EX 5: LA MEDIA DEI VOTI
-// Obiettivo: Calcola la somma di tutti i voti nell'array e poi 
+// Obiettivo: Calcola la somma di tutti i voti nell'array e poi
 // calcola la media (Somma totale diviso il numero di elementi).
 // Stampa sia la somma che la media.
 const voti = [24, 28, 30, 18, 22];
@@ -90,39 +89,36 @@ let sommaVoti = 0;
 
 // --- Scrivi qui il codice ---
 for (let i = 0; i < voti.length; i++) {
-    sommaVoti += voti[i]
+  sommaVoti += voti[i];
 }
 console.log(sommaVoti);
 console.log(sommaVoti / voti.length);
 
-
-
 // -----------------------------------------------------------------------------
-console.log("=============================================================================================================================================================");
-
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 // EX 6: TROVA L'INDICE (RICERCA MANUALE)
-// Obiettivo: Cicla l'array 'nomi'. Se trovi il nome "Marco", 
+// Obiettivo: Cicla l'array 'nomi'. Se trovi il nome "Marco",
 // stampa "Marco è in posizione: [i]".
 // Se non lo trovi, non stampare nulla.
 const nomi2 = ["Anna", "Luca", "Marco", "Sara"];
 
 // --- Scrivi qui il codice ---
 for (let i = 0; i < nomi2.length; i++) {
-    if (nomi2[i] === "Marco") {
-        console.log(`Marco è in posizione: ${i}`);        
-    }
+  if (nomi2[i] === "Marco") {
+    console.log(`Marco è in posizione: ${i}`);
+  }
 }
 
-
 // -----------------------------------------------------------------------------
-console.log("=============================================================================================================================================================");
-
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 // EX 7: IL FILTRO DEI NOMI CORTI
-// Obiettivo: Dato un array di città, crea un nuovo array 'cittaCorte' 
+// Obiettivo: Dato un array di città, crea un nuovo array 'cittaCorte'
 // che contenga solo le città con meno di 5 lettere.
 const citta = ["Roma", "Napoli", "Pisa", "Milano", "Asti"];
 const cittaCorte = [];
@@ -130,42 +126,41 @@ const cittaCorte = [];
 // --- Scrivi qui il codice ---
 
 for (let i = 0; i < citta.length; i++) {
-    if (citta[i].length < 5) {
-        cittaCorte.push(citta[i])
-    }
+  if (citta[i].length < 5) {
+    cittaCorte.push(citta[i]);
+  }
 }
 console.log(cittaCorte);
 
-
 // -----------------------------------------------------------------------------
-console.log("=============================================================================================================================================================");
-
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 // EX 8: IL RILEVATORE DI NUMERI NEGATIVI
-// Obiettivo: Cicla l'array 'temperature'. Se trovi ANCHE SOLO UN numero minore di 0, 
+// Obiettivo: Cicla l'array 'temperature'. Se trovi ANCHE SOLO UN numero minore di 0,
 // cambia la variabile 'allarme' in true e stampa "Pericolo gelo!".
 const temperature = [4, 12, -2, 8, 5];
 let allarme = false;
 
 // --- Scrivi qui il codice ---
 for (let i = 0; i < temperature.length; i++) {
-    if (temperature[i] < 0) {
-        allarme = true;
-        console.log("Pericolo gelo!");
-        
-    }
+  if (temperature[i] < 0) {
+    allarme = true;
+    console.log("Pericolo gelo!");
+  }
 }
 
-console.log("=============================================================================================================================================================");
-
+console.log(
+  "=============================================================================================================================================================",
+);
 
 /**
  * LIVELLO 3.4: LOGICA AVANZATA SU ARRAY SEMPLICI
  */
 
 // EX 13: IL CONFRONTO TRA LISTE
-// Obiettivo: Dati due array di numeri della stessa lunghezza, crea un terzo array 
+// Obiettivo: Dati due array di numeri della stessa lunghezza, crea un terzo array
 // 'risultato' che contenga la SOMMA dei numeri nelle stesse posizioni.
 // Esempio: [1, 2] + [10, 20] => [11, 22]
 const listaA = [5, 10, 15, 20];
@@ -174,16 +169,13 @@ const listaSommata = [];
 
 // --- Scrivi qui il codice ---
 
-
-
-
 // -----------------------------------------------------------------------------
-console.log("====================================================================================================================================================");
-
-
+console.log(
+  "====================================================================================================================================================",
+);
 
 // EX 14: IL RILEVATORE DI DOPPIONI (BASE)
-// Obiettivo: Dato un array di parole e una 'parolaChiave', conta quante volte 
+// Obiettivo: Dato un array di parole e una 'parolaChiave', conta quante volte
 // la parola chiave appare, ma stampa un messaggio SOLO se appare più di una volta.
 const archivio = ["Test", "Ciao", "Mela", "Test", "Uva", "Test"];
 const parolaChiave = "Test";
@@ -191,21 +183,21 @@ let contatoreChiave = 0;
 
 // --- Scrivi qui il codice ---
 for (let i = 0; i < archivio.length; i++) {
-    if (archivio[i] === parolaChiave) {
-        contatoreChiave += 1
-    }
+  if (archivio[i] === parolaChiave) {
+    contatoreChiave += 1;
+  }
 }
 if (contatoreChiave >= 2) {
-    console.log(contatoreChiave);
+  console.log(contatoreChiave);
 }
 
 // -----------------------------------------------------------------------------
-console.log("====================================================================================================================================================");
-
-
+console.log(
+  "====================================================================================================================================================",
+);
 
 // EX 15: IL FILTRO DEGLI INDICI PARI
-// Obiettivo: Dato l'array 'numeri', inserisci nell'array 'risultato' solo i numeri 
+// Obiettivo: Dato l'array 'numeri', inserisci nell'array 'risultato' solo i numeri
 // che si trovano in una POSIZIONE (indice i) pari.
 // Nota: Non stiamo guardando se il numero è pari, ma se lo è la sua posizione!
 const dati = [100, 200, 300, 400, 500, 600];
@@ -213,18 +205,16 @@ const risultatoPosizioniPari = [];
 
 // --- Scrivi qui il codice ---
 for (let i = 0; i < dati.length; i++) {
-    if (i % 2 === 0) {
-        risultatoPosizioniPari.push(dati[i]);
-    }
+  if (i % 2 === 0) {
+    risultatoPosizioniPari.push(dati[i]);
+  }
 }
 console.log(risultatoPosizioniPari);
 
-
-
 // -----------------------------------------------------------------------------
-console.log("====================================================================================================================================================");
-
-
+console.log(
+  "====================================================================================================================================================",
+);
 
 // EX 16: TROVA IL MINIMO (NON IL MASSIMO!)
 // Obiettivo: Trova il numero PIÙ PICCOLO all'interno dell'array 'temperature'.
@@ -233,60 +223,73 @@ const temperature2 = [15, 8, 22, 3, 10, 12];
 let min = temperature2[0];
 
 // --- Scrivi qui il codice ---
-for (let i= 0; i < temperature2.length; i++) {
-    if (temperature2[i] < min) {
-        min = temperature2[i];
-    }
+for (let i = 0; i < temperature2.length; i++) {
+  if (temperature2[i] < min) {
+    min = temperature2[i];
+  }
 }
 console.log(min);
 
-console.log("====================================================================================================================================================");
+console.log(
+  "====================================================================================================================================================",
+);
 
 /**
  * LIVELLO 3.5: ARRAY "BRAIN-MELTER"
-*/
+ */
 
 // EX 17: IL RILEVATORE DI "DOPPIONI GENERICI"
-// Obiettivo: Senza usare una parola chiave fissa, controlla se l'array 'numeri' 
+// Obiettivo: Senza usare una parola chiave fissa, controlla se l'array 'numeri'
 // contiene almeno un numero duplicato (anche se non sai quale).
 // Se trovi un duplicato, imposta 'haDuplicati' a true e ferma il ciclo.
-const numeriDuplicati = [10, 5, 8, 3, 5, 12];
+const numeriDuplicati = [10, 5, 8, 3, 12];
 let haDuplicati = false;
 
 // --- Scrivi qui il codice ---
 
+//for (let i = 0; i < numeriDuplicati.length; i++) {
+//    for (let d = i + 1; d < numeriDuplicati.length; d++) {
+//        if (numeriDuplicati[i] === numeriDuplicati[d]) {
+//            haDuplicati = true;
+//            break; //note esce dal ciclo interno
+//        }
+//    }
+//    if (haDuplicati) {
+//        break; //note esce dal ciclo esterno
+//    }
+//}
+//console.log(haDuplicati);
+
+const verificaDoppioni = [];
+
+//  numeriDuplicati[0] => numeriDuplicati[0]
+//  numeriDuplicati[0] => numeriDuplicati[1]
+//  numeriDuplicati[0] => numeriDuplicati[2]
+//  numeriDuplicati[0] => numeriDuplicati[3]
+//  numeriDuplicati[0] => numeriDuplicati[4]
+
+//  numeriDuplicati[1] => numeriDuplicati[0]
+//  numeriDuplicati[1] => numeriDuplicati[1]
+//  numeriDuplicati[1] => numeriDuplicati[2]
+//  numeriDuplicati[1] => numeriDuplicati[3]
+//  numeriDuplicati[1] => numeriDuplicati[4]
+
 for (let i = 0; i < numeriDuplicati.length; i++) {
-    for (let d = i + 1; d < numeriDuplicati.length; d++) {
-        if (numeriDuplicati[i] === numeriDuplicati[d]) {
-            haDuplicati = true;
-            break; //note esce dal ciclo interno
-        }
+  for (let d = 0; d < numeriDuplicati.length; d++) {
+    // console.log(
+    //   `${numeriDuplicati[i]} === ${numeriDuplicati[d]} && ${i} != ${d}`,
+    // );
+    if (numeriDuplicati[i] === numeriDuplicati[d] && i != d) {
+      haDuplicati = true;
     }
-    if (haDuplicati) {
-        break; //note esce dal ciclo esterno
-    }
+  }
 }
 console.log(haDuplicati);
 
-
-//note errata chiedere il perchè
-//^    for (let i = 0; i < numeriDuplicati.length; i++) {
-//^    const verificaDoppioni = [];
-//^        verificaDoppioni.push(numeriDuplicati[i]);
-//^        for (let d = 0; d < verificaDoppioni.length; d++) {
-//^            if (numeriDuplicati[d] === verificaDoppioni[d]) {
-//^                haDuplicati = true;
-//^            }
-//^        }
-//^    }
-//^    console.log(haDuplicati);
-
-
-
 // -----------------------------------------------------------------------------
-console.log("====================================================================================================================================================");
-
-
+console.log(
+  "====================================================================================================================================================",
+);
 
 // EX 18: LA DIFFERENZA MASSIMA
 // Obiettivo: Trova la differenza più grande tra due numeri consecutivi nell'array.
@@ -296,15 +299,13 @@ let diffMassima = 0;
 
 // --- Scrivi qui il codice ---
 
-
-
 // -----------------------------------------------------------------------------
-console.log("====================================================================================================================================================");
-
-
+console.log(
+  "====================================================================================================================================================",
+);
 
 // EX 19: UNIONE E FILTRO (SENZA DOPPIONI)
-// Obiettivo: Dati due array 'lista1' e 'lista2', crea un terzo array 'unione' 
+// Obiettivo: Dati due array 'lista1' e 'lista2', crea un terzo array 'unione'
 // che contenga tutti gli elementi, MA solo se non sono già presenti (niente doppioni nell'unione).
 // Suggerimento: usa .includes() per controllare se un elemento c'è già prima di fare .push().
 const lista1 = ["Mela", "Pera"];
@@ -313,12 +314,10 @@ const unioneSenzaDoppioni = [];
 
 // --- Scrivi qui il codice ---
 
-
-
 // -----------------------------------------------------------------------------
-console.log("====================================================================================================================================================");
-
-
+console.log(
+  "====================================================================================================================================================",
+);
 
 // EX 20: L'INVERTITORE DI POSIZIONI (SWAP)
 // Obiettivo: Dato un array di 2 elementi, scambia la loro posizione senza creare un nuovo array.
@@ -327,8 +326,6 @@ const coppia = ["Inizio", "Fine"];
 
 // --- Scrivi qui il codice ---
 
-
-
-
-
-console.log("====================================================================================================================================================");
+console.log(
+  "====================================================================================================================================================",
+);
